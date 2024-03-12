ba7d42dbf4663893f07ea360e0ce8a54fce03811
Alin Enachescu <alinenachescu033@gmail.com>
2024-03-12 22:29:38 +0200

    added currency enum

---
+++ include/currency.h
@@ -0,0 +1,12 @@
+#ifndef STOCK_EXCHANGE_TOOLS_CURRENCY_H
+#define STOCK_EXCHANGE_TOOLS_CURRENCY_H
+
+enum class Currency
+{
+    Unknown,
+    Ron,
+    Usd,
+    Eur,
+};
+
+#endif // STOCK_EXCHANGE_TOOLS_CURRENCY_H
